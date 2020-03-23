80c96d273cb727b1bcb124a89d24253653eb728a
Oscar <oscarhn1998@gmail.com>
2020-03-23 13:24:26 +0100

    asd

--- web_sstt.c
+++ web_sstt.c
@@ -59,18 +59,14 @@ void obtenerHeaderDate(char * date){
 }
 
 void sendHeaders(char * msgType, char * fileType, long int size, int socket_fd){
-	printf("Llega a la func\n");
 	char date[1000];
 	obtenerHeaderDate(date);
-	printf("Llega a cType\n");
 	char cType[1000];
 	sprintf(cType, "Content-type: %s\r\n", fileType);
-	printf("Llega a cLength\n");
 	char cLength [1000];
 	sprintf(cLength,"Content-length: %ld\r\n", size);
 	char headers[8000];
 	sprintf(headers, "%s%s%s%s\r\n", msgType, date, cType, cLength);
-	printf("Llega a write\n");
 	write(socket_fd, headers, strlen(headers));
 }
 
